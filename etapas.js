eab1c7b29badae973cc0e4b6ac8226ca540c053a
Aline Teixeira <alinet.reis1@gmail.com>
2021-08-30 17:48:30 -0300

    Criação campo imagem candidatos

---
+++ etapas.js
@@ -0,0 +1,39 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+              numero: 12345,
+              nome: 'Fulano de tal',
+              partido: 'ABC',
+              fotos: [{url:'imagem1.jpg', legenda: 'Vereador', small:false}]
+            },
+
+            {numero: 12345,
+              nome: 'Fulano de tal',
+              partido: 'ABC',
+              fotos: [{url:'imagem1.jpg', legenda: 'Vereador', small:false}]
+            },  
+        ]
+    },
+
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+              numero: 12,
+              nome: 'Fulano de tal',
+              partido: 'ABC',
+              fotos: [{url:'imagem2.jpg', legenda: 'Prefeito', small:true}]
+            },
+
+            {numero: 13,
+              nome: 'Fulano de tal',
+              partido: 'ABC',
+              fotos: [{url:'imagem2.jpg', legenda: 'Prefeito', small:true}]
+            },  
+        ]
+    },
+]
